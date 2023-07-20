927f1ff910d7a7ab8a48f2c51a1e1f1cbaf0aa8b
AsadbekKazakovDev <asadbekkazakov5300@gmail.com>
2023-07-20 08:11:55 +0500

    solve main

--- find_all_users_id.py
+++ find_all_users_id.py
@@ -1,6 +1,6 @@
 from read_data import read_data
 
-def find_all_users_id(data: dict)->list:
+def find_all_users_id(ans: dict)->list:
     """ 
     This function will find all the users in the json file and return the list of users id
     
@@ -9,11 +9,12 @@ def find_all_users_id(data: dict)->list:
     Returns:
         list: List containing all the users id
     """
-    data = read_data("data/result.json")
+    ans= read_data("data/result.json")
     lst = []
-    for user in data["messages"]:
-        lst.append(user["id"])
+    for user in ans["messages"]:
+        if user["type"]=='message':
+            lst.append(user["id"])
     
     return lst
-data = ("dats/result.json")
-print(find_all_users_id(data))
+ans = ("data/result.json")
+print(find_all_users_id(ans))
